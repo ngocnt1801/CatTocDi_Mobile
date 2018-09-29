6f0fa05c1be48032439c776cc65ad5b6a5cdc0d1
ngoc <ngocnt1801@gmail.com>
2018-09-29 20:34:16 +0700

    add appointment fragment

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/fragements/AppointmentFragment.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/fragements/AppointmentFragment.java
@@ -3,17 +3,23 @@ package com.salon.cattocdi.fragements;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.salon.cattocdi.R;
+import com.salon.cattocdi.adapters.FragementAppointmentTestAdapter;
+import com.salon.cattocdi.adapters.TestRecycleViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AppointmentFragment extends Fragment {
 
+    RecyclerView rvUpcomming;
 
     public AppointmentFragment() {
         // Required empty public constructor
@@ -24,7 +30,20 @@ public class AppointmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appointment, container, false);
+        View view = inflater.inflate(R.layout.fragment_appointment, container, false);
+        rvUpcomming = view.findViewById(R.id.fg_appointment_rv_upcoming);
+        testRecycleViewAdapter(rvUpcomming);
+        return view;
+    }
+
+    private void testRecycleViewAdapter(RecyclerView rv){
+        //Show RECYCLEVIEW
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        rv.setLayoutManager(mLayoutManager);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        FragementAppointmentTestAdapter adapter = new FragementAppointmentTestAdapter();
+        rv.setAdapter(adapter);
     }
 
 }
