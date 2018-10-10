417b04abff46422ab3599f357823f738e6baf283
tiendat <tiendatdeveloper1997@gmail.com>
2018-10-10 11:27:48 +0700

    Tien Dat - Add Google Map to Salon Detail

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/SalonDetailActivity.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/SalonDetailActivity.java
@@ -6,10 +6,22 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.WindowManager;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.salon.cattocdi.adapters.SalonProfileGalleryRecycleViewAdapter;
 import com.salon.cattocdi.adapters.ServiceRecycleViewAdapter;
 
-public class SalonDetailActivity extends AppCompatActivity {
+public class SalonDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMapOptions options = new GoogleMapOptions().liteMode(true);
+    private GoogleMap map;
+    private LatLng latLng;
+    private SupportMapFragment supportMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +36,20 @@ public class SalonDetailActivity extends AppCompatActivity {
         RecyclerView galleryRecycleView = findViewById(R.id.salon_image_recycle_view);
         galleryRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         galleryRecycleView.setAdapter(new SalonProfileGalleryRecycleViewAdapter());
+//        10.8459529,106.6322835
+        latLng = new LatLng(10.8466881,106.6329596);
+        supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.salon_detail_map);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+        map.addMarker(markerOptions);
     }
 }
