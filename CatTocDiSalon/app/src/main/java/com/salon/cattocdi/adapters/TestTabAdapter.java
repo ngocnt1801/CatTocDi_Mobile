9bdd20b83aa4f4948b96f5dee166f3b472fa97e4
ngoc <ngocnt1801@gmail.com>
2018-10-13 22:08:12 +0700

    set review for tab salon detail

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/adapters/TestTabAdapter.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/adapters/TestTabAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.salon.cattocdi.fragements.ReviewsFragment;
 import com.salon.cattocdi.fragements.SalonDetailContactFragment;
 import com.salon.cattocdi.fragements.SalonDetailServiceFragment;
 import com.salon.cattocdi.fragements.TestTabFragment;
@@ -24,7 +25,7 @@ public class TestTabAdapter extends FragmentPagerAdapter{
             case 1:
                 return new SalonDetailContactFragment();
             case 2:
-                return TestTabFragment.getInstance(position);
+                return new ReviewsFragment();
         }
         return null;
     }
