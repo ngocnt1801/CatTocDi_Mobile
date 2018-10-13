b3c861717599d213dff59f48b917a3a58692876a
tiendat <tiendatdeveloper1997@gmail.com>
2018-10-13 15:48:19 +0700

    Change Salon Detail to Tab View

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/SalonDetailActivity.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/SalonDetailActivity.java
@@ -1,9 +1,12 @@
 package com.salon.cattocdi;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,12 +19,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.salon.cattocdi.adapters.SalonProfileGalleryRecycleViewAdapter;
 import com.salon.cattocdi.adapters.ServiceRecycleViewAdapter;
+import com.salon.cattocdi.adapters.TestTabAdapter;
 
-public class SalonDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private GoogleMapOptions options = new GoogleMapOptions().liteMode(true);
-    private GoogleMap map;
-    private LatLng latLng;
-    private SupportMapFragment supportMapFragment;
+public class SalonDetailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +33,21 @@ public class SalonDetailActivity extends AppCompatActivity implements OnMapReady
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_salon_detail);
-        RecyclerView serviceRecycleView = findViewById(R.id.salon_service_recycle_view);
-        serviceRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        serviceRecycleView.setAdapter(new ServiceRecycleViewAdapter());
+        // SET UP TAB LAYOUT
+        viewPager = (ViewPager) findViewById(R.id.detail_pager);
+        TestTabAdapter adapter = new TestTabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.detail_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+        // END SET UP TAB LAYOUT
 
-        RecyclerView galleryRecycleView = findViewById(R.id.salon_image_recycle_view);
-        galleryRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        galleryRecycleView.setAdapter(new SalonProfileGalleryRecycleViewAdapter());
-//        10.8459529,106.6322835
-        latLng = new LatLng(10.8466881,106.6329596);
-        supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.salon_detail_map);
-        supportMapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.map = googleMap;
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
-        map.addMarker(markerOptions);
+//        RecyclerView serviceRecycleView = findViewById(R.id.salon_service_recycle_view);
+//        serviceRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        serviceRecycleView.setAdapter(new ServiceRecycleViewAdapter());
+//
+//        RecyclerView galleryRecycleView = findViewById(R.id.salon_image_recycle_view);
+//        galleryRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+//        galleryRecycleView.setAdapter(new SalonProfileGalleryRecycleViewAdapter());
+////        10.8459529,106.6322835
     }
 }
---
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/TestTabActivity.java
@@ -0,0 +1,29 @@
+package com.salon.cattocdi;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+import com.salon.cattocdi.adapters.TestTabAdapter;
+
+public class TestTabActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_test_tab);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        TestTabAdapter adapter = new TestTabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
