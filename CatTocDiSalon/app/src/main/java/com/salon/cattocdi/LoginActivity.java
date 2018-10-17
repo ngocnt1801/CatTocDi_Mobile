44e46340fbbd6a97eb5f5784508ad79ceaf3364c
ngoc <ngocnt1801@gmail.com>
2018-10-17 09:19:40 +0700

    create login page and splash activity add color

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/LoginActivity.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/LoginActivity.java
@@ -5,24 +5,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.salon.cattocdi.utils.MyContants;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
