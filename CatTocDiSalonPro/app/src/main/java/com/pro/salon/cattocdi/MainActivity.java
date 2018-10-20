5f3a8b3c2144abfab235a7456f648eef1d2501f8
ngoc <ngocnt1801@gmail.com>
2018-10-20 13:44:36 +0700

    add naviagtion bar

--- CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/MainActivity.java
+++ CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pro.salon.cattocdi;
 
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +17,11 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,78 +33,101 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scheduleTable = findViewById(R.id.salon_schedule);
+//        scheduleTable = findViewById(R.id.salon_schedule);
+//
+//        scheduleTable.setOnTimeItemClickListener(new TimeTableItemViewHolder.OnTimeItemClickListener() {
+//            @Override
+//            public void onTimeItemClick(View view, int i, TimeGridData timeGridData) {
+//                Log.d("CLICK NE", "" + timeGridData.getTime().getTitle());
+//            }
+//        });
+//
+//        scheduleTable.setStartHour(7);
+//        scheduleTable.setTableMode(TimeTableView.TableMode.SHORT);
+//
+//        //assign data test
+//        ArrayList<TimeTableData> table = null;
+//        table = getSamples(DateTime.now().withTimeAtStartOfDay().getMillis());
+//        scheduleTable.setTimeTable(DateTime.now().withTimeAtStartOfDay().getMillis(), table);
 
-        scheduleTable.setOnTimeItemClickListener(new TimeTableItemViewHolder.OnTimeItemClickListener() {
-            @Override
-            public void onTimeItemClick(View view, int i, TimeGridData timeGridData) {
-                Log.d("CLICK NE", "" + timeGridData.getTime().getTitle());
+    }
+
+    private ArrayList<TimeTableData> getSamples(long date){
+
+        ArrayList<TimeTableData> tables = new ArrayList<>();
+        for(int i=0; i< 7; i++){
+            ArrayList<TimeData> values = new ArrayList<>();
+            DateTime start = new DateTime(date);
+            DateTime end = start.plusMinutes((int)((Math.random() * 10) + 1) * 30);
+            for(int j=0; j< 7; j++){
+                int textColor = R.color.black;
+
+
+                TimeData timeData = new TimeData(j, "", R.color.light, textColor, start.getMillis(), end.getMillis());
+
+                values.add(timeData);
+
+                start = end.plusMinutes((int)((Math.random() * 10) + 1) * 10);
+                end = start.plusMinutes((int)((Math.random() * 10) + 1) * 30);
             }
-        });
 
-        scheduleTable.setStartHour(7);
-        scheduleTable.setShowHeader(true);
-        scheduleTable.setTableMode(TimeTableView.TableMode.SHORT);
-
-        //assign data test
-        ArrayList<TimeTableData> table = getData();
-
-        scheduleTable.setTimeTable(DateTime.now().withTimeAtStartOfDay().getMillis(), table);
-
-    }
-
-    private ArrayList<TimeTableData> getData(){
-
-        Random random = new Random();
-        ArrayList<TimeTableData> table = new ArrayList<>();
-
-        for(int i = 0; i < 40; i++){
-            ArrayList<TimeData> col = new ArrayList<>();
-
-
-            TimeData t = new TimeData(0, "",
-                        R.color.color_table_1,
-                        R.color.lightTextColor,
-                        getMillis("2018-10-19 7:00:00"), getMillis("2018-10-19 8:30:00")) ;
-
-            TimeData t1 = new TimeData(1, "",
-                    R.color.color_table_2,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 9:00:00"), getMillis("2018-10-19 10:00:00")) ;
-            TimeData t2 = new TimeData(1, "",
-                    R.color.color_table_3,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 11:00:00"), getMillis("2018-10-19 11:45:00")) ;
-            TimeData t3 = new TimeData(1, "",
-                    R.color.color_table_4,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 12:00:00"), getMillis("2018-10-19 13:20:00")) ;
-            TimeData t4 = new TimeData(1, "",
-                    R.color.color_table_5,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 13:30:00"), getMillis("2018-10-19 14:15:00")) ;
-            TimeData t5 = new TimeData(1, "",
-                    R.color.color_table_6,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 15:00:00"), getMillis("2018-10-19 16:30:00")) ;
-            TimeData t6 = new TimeData(1, "",
-                    R.color.color_table_7,
-                    R.color.lightTextColor,
-                    getMillis("2018-10-19 17:00:00"), getMillis("2018-10-19 19:00:00")) ;
-
-                col.add(t);
-                col.add(t1);
-                col.add(t2);
-                col.add(t3);
-                col.add(t4);
-                col.add(t5);
-                col.add(t6);
-
-            table.add(new TimeTableData("", col));
+            tables.add(new TimeTableData("", values));
         }
-
-        return table;
+        return tables;
     }
+
+//    private ArrayList<TimeTableData> getData(){
+//
+//        Random random = new Random();
+//        ArrayList<TimeTableData> table = new ArrayList<>();
+//
+//        for(int i = 0; i < 7; i++){
+//            ArrayList<TimeData> col = new ArrayList<>();
+//
+//
+//            TimeData t = new TimeData(0, "",
+//                        R.color.color_table_1,
+//                        R.color.lightTextColor,
+//                        getMillis("2018-10-19 7:00:00"), getMillis("2018-10-19 8:30:00")) ;
+//
+//            TimeData t1 = new TimeData(1, "",
+//                    R.color.color_table_2,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 9:00:00"), getMillis("2018-10-19 10:00:00")) ;
+//            TimeData t2 = new TimeData(1, "",
+//                    R.color.color_table_3,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 11:00:00"), getMillis("2018-10-19 11:45:00")) ;
+//            TimeData t3 = new TimeData(1, "",
+//                    R.color.color_table_4,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 12:00:00"), getMillis("2018-10-19 13:20:00")) ;
+//            TimeData t4 = new TimeData(1, "",
+//                    R.color.color_table_5,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 13:30:00"), getMillis("2018-10-19 14:15:00")) ;
+//            TimeData t5 = new TimeData(1, "",
+//                    R.color.color_table_6,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 15:00:00"), getMillis("2018-10-19 16:30:00")) ;
+//            TimeData t6 = new TimeData(1, "",
+//                    R.color.color_table_7,
+//                    R.color.lightTextColor,
+//                    getMillis("2018-10-19 17:00:00"), getMillis("2018-10-19 19:00:00")) ;
+//
+//                col.add(t);
+//                col.add(t1);
+//                col.add(t2);
+//                col.add(t3);
+//                col.add(t4);
+//                col.add(t5);
+//                col.add(t6);
+//
+//            table.add(new TimeTableData("", col));
+//        }
+//
+//        return table;
+//    }
     private long getMillis(String day){
         DateTime date = getDateTimePattern().parseDateTime(day);
         return date.getMillis();
