d54297f69ec4c2a974260ec28d10c41c5f760e01
ngoc <ngocnt1801@gmail.com>
2018-10-24 14:04:17 +0700

    change data hard

--- CatTocDiSalon/app/src/main/java/com/salon/cattocdi/adapters/FragementAppointmentTestAdapter.java
+++ CatTocDiSalon/app/src/main/java/com/salon/cattocdi/adapters/FragementAppointmentTestAdapter.java
@@ -75,6 +75,7 @@ public class FragementAppointmentTestAdapter extends RecyclerView.Adapter<Fragem
         if (i == 4) {
             viewHolder.tvDate.setText("10/10/2018");
             viewHolder.tvAppoinmentType.setText("Cuộc hẹn đã qua");
+            viewHolder.appointmentRl.setBackgroundColor(Color.parseColor("#eeeeee"));
         }
 
 
